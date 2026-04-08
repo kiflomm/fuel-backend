d1fe8391d6f4acc09271f352f4e9f29d415dbd5e
kiflom <kiflomberihu2024@gmail.com>
2026-04-08 19:32:46 +0300

    Add cookie-parser dependency and integrate it into the main application for handling cookies; update package.json and pnpm-lock.yaml accordingly.

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule ,DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   
-
-  const config = new DocumentBuilder()
-  .setTitle('API Documentation')
-  .setDescription('API documentation for the application')
-  .setVersion('1.0')
-  .addServer(process.env.SERVER_URL || 'http://localhost:3000', 'Development server')
-  .addBearerAuth(
-    {
-      type: 'http',
-      scheme: 'bearer',
-      bearerFormat: 'JWT',
-      name: 'JWT',
-      description: 'Enter JWT token',
-      in: 'header',
-    },
-    'JWT-auth',
-  )
-  .build();
-
-
   const app = await NestFactory.create(AppModule);
-    // Enable CORS with credentials
-    app.enableCors({
-      origin: process.env.APP_URL || 'http://localhost:3001',
-      credentials: true,
-    });
+  // Enable CORS with credentials
+  app.enableCors({
+    origin: process.env.APP_URL || 'http://localhost:3001',
+    credentials: true,
+  });
+  // Add cookie parser
+  app.use(cookieParser());
+
+  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
