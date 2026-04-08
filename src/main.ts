ceb48589ba6bdac60e2b97d45e23d7a604363cb8
kiflom <kiflomberihu2024@gmail.com>
2026-04-08 19:53:45 +0300

    Add class-transformer dependency and integrate global validation pipe in main application for improved data validation; update package.json and pnpm-lock.yaml accordingly.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,37 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Fuel Monitor API')
+    .setDescription('API documentation for Fuel Monitor backend')
+    .setVersion('1.0.0')
+    .addServer(process.env.SERVER_URL || 'http://localhost:3000', 'Development server')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth', // This name here is important for matching up with @ApiBearerAuth() in your controller!
+    )
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, documentFactory, {
+      swaggerOptions: {
+        persistAuthorization: true,
+      },
+    }
+  );
+
   // Enable CORS with credentials
   app.enableCors({
     origin: process.env.APP_URL || 'http://localhost:3001',
@@ -14,7 +40,14 @@ async function bootstrap() {
   // Add cookie parser
   app.use(cookieParser());
 
-  
+  // Global validation pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
